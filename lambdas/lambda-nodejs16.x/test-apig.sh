71f5daf8a23516f0385fcb196fe1bfee025f55e3
anzharip <10259593+anzharip@users.noreply.github.com>
2022-06-29 09:54:14 +0900

    fix: test file name and import change

---
+++ lambdas/lambda-nodejs16.x/test-apig.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sam local invoke "HelloWorldFunction" -e ../../test/fixture/event-apig.json
--- lambdas/lambda-nodejs16.x/test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sam local invoke "HelloWorldFunction" -e ../../test/fixture/event.json
