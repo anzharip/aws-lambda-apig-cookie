cc7f2e5cc3d7c03dff54f89de5a8e23602e4ec00
anzharip <10259593+anzharip@users.noreply.github.com>
2022-06-29 09:23:38 +0900

    fix: remove unused types

--- src/types/types.type.ts
+++ src/types/types.type.ts
@@ -19,24 +19,4 @@ type CookieResult = {
   [key: string]: Array<string> | Array<setCookieParser.Cookie>;
 };
 
-enum CookieAttribute {
-  Comment = "Comment",
-  CommentURL = "CommentURL",
-  Discard = "Discard",
-  Domain = "Domain",
-  Expires = "Expires",
-  MaxAge = "MaxAge",
-  Origin = "Origin",
-  Path = "Path",
-  Port = "Port",
-  SameSite = "SameSite",
-  Secure = "Secure",
-  Version = "Version",
-}
-
-type ParsedCookie = {
-  name: string;
-  value: string;
-};
-
-export { ApiGatewayHttpApiProxyEvent, CookieResult, CookieAttribute };
+export { ApiGatewayHttpApiProxyEvent, CookieResult };
